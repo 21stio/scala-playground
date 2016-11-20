fe74971b6b9a269ae2aa1f943d4e6d17bb9734bf
21stio <ph.woerdehoff+21st@gmail.com>
2016-11-20 17:54:07 +0100

    automated backup commit

--- src/HelloWorld.scala
+++ src/HelloWorld.scala
@@ -1,3 +1,30 @@
+trait TModel {
+  def getName: String
+}
+
+trait TRepository {
+  def getModels: List[TModel]
+}
+
+class PetModel(name: String) extends TModel {
+  override def getName: String = name
+}
+
+class PetRepository(pets: List[TModel]) extends TRepository {
+  override def getModels: List[TModel] = pets
+
+  def printNames() =
+    for (model <- getModels) print(model.getName)
+}
+
 object HelloWorld extends App {
-    print("Hello World!")
+  val pets: List[TModel] = List(
+    new PetModel("Peter"),
+    new PetModel("Hans"),
+    new PetModel("Wurst")
+  )
+
+  val repository: PetRepository = new PetRepository(pets)
+
+  repository.printNames()
 }
